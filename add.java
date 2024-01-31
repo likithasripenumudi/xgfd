5e553cb89110ce6c75e6b86134b675bfb609b438
likithasripenumudi <158254164+likithasripenumudi@users.noreply.github.com>
2024-01-31 11:45:21 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class addition
+{
+static public void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.print("enter a value:");
+int a=sc.nextInt();
+System.out.print("enter b value:");
+int b=sc.nextInt();
+System.out.println("sum is "+(a+b));
+}
+}
